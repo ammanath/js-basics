25e5df21c824a4c06ff772f2a0b70438dc812aa6
brijesh <github@ammanath.com>
2021-07-07 09:59:33 +0530

    objects

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let name1 = 'Robert';
+let age = 45;
+let isApproved = true;
+let space;
+let last = null;
+
+const interestRate = 6.7;
+
+let person = {
+    name:'Robert',
+    age:45
+};
+
+//Dot notation
+person.name='Michael';
+
+//Bracket notation
+person['name'] = 'Mary';
+
+
+//Primitive = String, Number, Boolean, undefined, null
+//Reference Types = Object, Array, Function
+console.log(`The values are : ${name1} / ${age} / 
+ ${isApproved} / ${space} / ${last}`);
+
+ console.log(person);
