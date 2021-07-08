5314487f97ae3464028b4f72f6e2df96357ea98c
ammanath <akbrijesh@gmail.com>
2021-07-08 09:25:30 +0530

    constructors

--- objects.js
+++ objects.js
@@ -10,3 +10,29 @@ draw:function(){
 };
 
 circle.draw();
+
+//Factory function
+function createCircle(radius){
+    return{
+        radius,
+        draw:function(){
+            console.log('drawing from factory');
+        }
+    };
+}
+
+const circle1 = createCircle(2);
+circle1.draw();
+
+//constructor function
+function Circle(radius){
+    this.radius = radius;
+    this.draw= function(){
+        console.log('draw from constructor');
+    }
+}
+
+const circle2 = new Circle(2);
+circle2.draw();
+
+let x= {};
