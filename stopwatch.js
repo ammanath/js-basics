f4fbc1dc9a1a47efbda7cc26e03b4d080eb02e41
ammanath <akbrijesh@gmail.com>
2021-07-11 09:50:14 +0530

    stopwatch changes

--- stopwatch.js
+++ stopwatch.js
@@ -1,14 +1,26 @@
 
 function StopWatch(){
-    this.duration = 0;
+    let duration = 0;
+    let intervalId;
     this.start = function(){
+        intervalId = setInterval(setDuration,1000);
         console.log('Started...');
     };
     this.stop=function(){
+        clearInterval(intervalId);
         console.log('Stopped!');
     }
     this.reset=function(){
-        this.duration = 0;
-        console.log('Reset');
+        duration = 1;
+        clearInterval(intervalId);
+        console.log('Reset : ' + duration);
+    }
+
+    this.printInterval = function(){
+        console.log('Duration : ' +  duration);
+    }
+
+    let setDuration = function(){
+        duration = duration + 1;
     }
 }
