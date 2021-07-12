0d37d4cacf94b7bd0c5820f5db605d63e979a828
ammanath <akbrijesh@gmail.com>
2021-07-12 10:57:16 +0530

    basic class

---
+++ classes.js
@@ -0,0 +1,17 @@
+class Car{
+    constructor(doors, engine, color){
+        this.doors= doors;
+        this.engine= engine;
+        this.color = color;
+    }
+
+    carStats(){
+        return `This car has ${this.doors} doors, 
+            a ${this.engine} engine and is of
+             ${this.color} color`;
+    }
+}
+
+const cx5 = new Car(4, 'V6', 'Orange');
+console.log(cx5);
+console.log(cx5.carStats());
